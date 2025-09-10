eed09791661819e152549abd834cf9e42400c326
Imran Shaikh <shaikhimran65625@gmail.com>
2025-09-10 15:08:46 +0530

    Fix API base URL to use deployed backend

--- src/utils/apiPoint.js
+++ src/utils/apiPoint.js
@@ -7,6 +7,6 @@
 // export const COMPANY_API_POINT="http://localhost:8000/api/v1/company"
 
 export const USER_API_END_POINT = `${import.meta.env.VITE_API_URL}/api/v1/user`;
-export const COMPANY_API_END_POINT = `${import.meta.env.VITE_API_URL}/api/v1/company`;
-export const JOB_API_END_POINT = `${import.meta.env.VITE_API_URL}/api/v1/job`;
-export const APPLICATION_API_END_POINT = `${import.meta.env.VITE_API_URL}/api/v1/application`;
+export const COMPANY_API_POINT = `${import.meta.env.VITE_API_URL}/api/v1/company`;
+export const ALL_JOB_API_POINT = `${import.meta.env.VITE_API_URL}/api/v1/job`;
+export const APPLICATION_JOB_API_POINT = `${import.meta.env.VITE_API_URL}/api/v1/application`;
