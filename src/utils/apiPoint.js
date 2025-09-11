aa1cde7ded9206530e1599b16ca3e8a69ad39db4
Imran Shaikh <shaikhimran65625@gmail.com>
2025-09-11 20:00:30 +0530

    Fix API endpoints

--- src/utils/apiPoint.js
+++ src/utils/apiPoint.js
@@ -7,9 +7,9 @@
 // export const COMPANY_API_POINT="http://localhost:8000/api/v1/company"
 
 
-// src/utils/apiPoint.js
 
 const API_BASE_URL = import.meta.env.VITE_API_URL;
+// Your .env file: VITE_API_URL=https://job-portal-backend-78er.onrender.com
 
 export const USER_API_END_POINT = `${API_BASE_URL}/api/v1/user`;
 export const ALL_JOB_API_POINT = `${API_BASE_URL}/api/v1/job`;
